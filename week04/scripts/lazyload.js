3de4427b8c4b8cddb191d4f6f109b8f36f5c6494
ToliTorres <toliluz@gmail.com>
2025-11-26 04:52:58 -0600

    created Learning Activity Lazyloading Images activity

---
+++ week04/scripts/lazyload.js
@@ -0,0 +1,3 @@
+// Display current year and last modified date
+document.getElementById("currentyear").textContent = new Date().getFullYear();
+document.getElementById("lastModified").textContent = `Last modified: ${document.lastModified}`;
